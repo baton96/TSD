0e0ff21d4c49a64ee26b0fa4a9cf5e1a3a21b2ae
baton96 <podolce0@gmail.com>
2020-04-16 21:31:35 +0200

    Added WPF

---
+++ 05-WPF/Books.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary
+{
+    public class Book : INotifyPropertyChanged
+    {
+        public static int counter = 6;
+        public int Id { get; private set; }
+        //public string Title { get; set; }
+        //public string Author { get; set; }
+        //public bool IsRead { get; set; }
+        //public int Year { get; set; }
+        //public BookFormat Format { get; set; }
+        public Book(int id) { Id = id; }
+        public Book() { Id = counter++; }
+        private string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                NotifyPropertyChanged("Title");
+            }
+        }
+
+        private string author;
+        public string Author
+        {
+            get => author;
+            set
+            {
+                author = value;
+                NotifyPropertyChanged("Author");
+            }
+        }
+
+        private bool isRead;
+        public bool IsRead
+        {
+            get => isRead;
+            set
+            {
+                isRead = value;
+                NotifyPropertyChanged("IsRead");
+            }
+        }
+
+        private int year;
+        public int Year
+        {
+            get => year;
+            set
+            {
+                year = value;
+                NotifyPropertyChanged("Year");
+            }
+        }
+
+        //public readonly List<BookFormat> BookFormats = new List<BookFormat>(){ BookFormat.PaperBack, BookFormat.EBook };
+        public static List<BookFormat> BookFormats = new List<BookFormat>() { BookFormat.PaperBack, BookFormat.EBook };
+        public BookFormat format;
+        public BookFormat Format
+        {
+            get => format;
+            set
+            {
+                format = value;
+                NotifyPropertyChanged("Format");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+    public class Book2
+    {
+        public int Id { get; private set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public int Year { get; set; }
+
+        public BookFormat Format { get; set; }
+        public Book2(int id)
+        {
+            Id = id;
+
+        }
+    }
+
+    public enum BookFormat
+    {
+        PaperBack, EBook
+    }
+
+    public static class MyBookCollection
+    {
+        public static List<Book> GetMyCollection()
+        {
+            return new List<Book>()
+            {
+                new Book(1){ Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Philosopher's Stone", Year=1997},
+
+                new Book(2)
+                {
+                    Author = "J.K. Rowling", Format = BookFormat.EBook, IsRead = true, Title = "Harry Potter and the Chamber of Secrets",
+                    Year = 1998
+                },
+
+                new Book(3){ Author = "J.K. Rowling", Format = BookFormat.PaperBack, IsRead = true, Title = "Harry Potter and the Prisoner of Azkaban", Year = 1999},
+
+                new Book(4){ Author = "Jonathan Swift", Format = BookFormat.PaperBack, IsRead = false, Title = "Very Long Title", Year=1972},
+
+                new Book(5){Author = "Wayne Thomas Batson", Format = BookFormat.EBook, IsRead = true, Title = "Isle of Swords", Year = 2007},
+
+                new Book(5){Author = "Louis A. Meyer", Format = BookFormat.EBook, IsRead = true, Title = "Under the Jolly Roger", Year = 200},                
+            };
+
+        }
+    }
+}
---
+++ 05-WPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using HomeLibrary;
+using System;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+		private readonly ObservableCollection<Book> books = new ObservableCollection<Book>(MyBookCollection.GetMyCollection());
+		public MainWindow()
+        {
+            InitializeComponent();
+			lbBooks.ItemsSource = books;
+		}		
+
+		private void Add(object sender, RoutedEventArgs e)
+		{
+			books.Add(new Book());			
+		}
+
+		private void Delete(object sender, RoutedEventArgs e)
+		{
+			if (MessageBox.Show(
+				"This book will be removed",
+				"Book removal",
+				MessageBoxButton.OKCancel,
+				MessageBoxImage.Warning,
+				MessageBoxResult.Cancel) == MessageBoxResult.OK)
+			{
+				books.Remove(lbBooks.SelectedItem as Book);
+			}
+
+		}
+
+		private void TitleChanged(object sender, RoutedEventArgs e)
+		{
+			(lbBooks.SelectedItem as Book).Title = (sender as TextBox).Text;	
+		}
+
+		private void AuthorChanged(object sender, RoutedEventArgs e)
+		{
+			(lbBooks.SelectedItem as Book).Author = (sender as TextBox).Text;
+		}
+		private void IsReadChanged(object sender, RoutedEventArgs e)
+		{
+			(lbBooks.SelectedItem as Book).IsRead = (sender as CheckBox).IsChecked ?? false;
+		}
+		private void YearChanged(object sender, RoutedEventArgs e)
+		{
+			(lbBooks.SelectedItem as Book).Year = int.Parse((sender as TextBox).Text);
+		}
+		private void FormatChanged(object sender, RoutedEventArgs e)
+		{
+				(lbBooks.SelectedItem as Book).Format =
+				(BookFormat)Enum.Parse(typeof(BookFormat),
+					(sender as ComboBox)
+					.SelectedValue
+					.ToString()
+					.Split(' ')
+					[1]
+				);
+		}
+
+		private void Select(object sender, RoutedEventArgs e)
+		{
+			lbBook.ItemsSource = new ObservableCollection<Book>() { lbBooks.SelectedItem as Book };
+		}
+
+		private void Select(object sender, MouseButtonEventArgs e)
+		{
+			lbBook.ItemsSource = new ObservableCollection<Book>() { lbBooks.SelectedItem as Book };
+		}
+
+		private void SliderChanged(object sender, RoutedEventArgs e)
+		{
+			topButton.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0, byte.Parse((sender as TextBox).Text), 0));
+		}		
+	}
+}
